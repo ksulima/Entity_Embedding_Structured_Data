34b0da3320d2e2c9c5325516bb8e53585e27e493
Krzysztof Sulima <krzysztof.sulima1991@gmail.com>
2019-01-12 18:20:14 +0100

    reogranisation

---
+++ imports.py
@@ -0,0 +1,20 @@
+#imports
+
+import pandas as pd
+import numpy as np
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from IPython.display import HTML, display
+
+from pandas_summary import DataFrameSummary
+import re
+import os
+import pickle
+import math
+
+
+
+
